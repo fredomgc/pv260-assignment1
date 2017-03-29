024681e33408689a404c110e0453de81bdb5e19d
Dominik Veselý <dominik40@seznam.cz>
2017-03-29 14:24:07 +0200

    added initial prefill of default color to the PositionListFrame avoiding situations where colors might by NULL

--- src/main/java/cz/muni/tron/PositionsListFrame.java
+++ src/main/java/cz/muni/tron/PositionsListFrame.java
@@ -5,14 +5,19 @@ import cz.muni.tron.engine.InvalidPositionException;
 import java.awt.Color;
 import java.util.List;
 
-
 public class PositionsListFrame implements GameFrame {
+
     private final Color[][] frame;
 
-    public PositionsListFrame(int width, int height) {
+    public PositionsListFrame(int width, int height, Color defaultColor) {
         this.frame = new Color[height][width];
+        fillFrame(defaultColor);
     }
     
+    public PositionsListFrame(int width, int height) {
+        this(width, height, Color.BLACK);
+    }
+
     @Override
     public int getWidth() {
         return frame[0].length;
@@ -28,31 +33,39 @@ public class PositionsListFrame implements GameFrame {
         checkPositionIsValid(position);
         return frame[position.getColumn()][position.getRow()];
     }
-    
+
     public void addList(List<Position> positions, Color color) throws InvalidPositionException {
         for (Position position : positions) {
             addPosition(position, color);
         }
     }
-    
-    private void addPosition(Position position, Color color) throws InvalidPositionException {
+
+    public void addPosition(Position position, Color color) throws InvalidPositionException {
         checkPositionIsValid(position);
         frame[position.getColumn()][position.getRow()] = color;
     }
-    
+
     private void checkPositionIsValid(Position position) throws InvalidPositionException {
         if (!isPositionValid(position)) {
             throw new InvalidPositionException("Position " + position + " is out of the frame bound " + getFrameBounds());
         }
     }
-    
+
     private boolean isPositionValid(Position position) {
-        return position.getRow() >= 0 && position.getRow() < frame.length &&
-                position.getColumn() >= 0 && position.getColumn() < frame[0].length;
+        return position.getRow() >= 0 && position.getRow() < frame.length
+                && position.getColumn() >= 0 && position.getColumn() < frame[0].length;
     }
-    
+
     private String getFrameBounds() {
         return "[" + frame.length + "; " + frame[0].length + "]";
+    }
+    
+    private void fillFrame(Color color) {
+        for (int row = 0; row < getHeight(); row++) {
+            for (int column = 0; column < getWidth(); column++) {
+                addPosition(new Position(row, column), color);
+            }
+        }
     }
 
 }
